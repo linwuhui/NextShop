e1525c2852125164a2b39b5a2d4ffe73489ca0c8
Breeze <2786515248@qq.com>
2024-09-20 00:48:59 +0800

    something chaneg for Migration generate db file of sqlite in project

--- NextShop.API/Data/Migration/20240919164053_InitialCreate.cs
+++ NextShop.API/Data/Migration/20240919164053_InitialCreate.cs
@@ -5,7 +5,7 @@
 namespace NextShop.API.Data.Migration
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class InitialCreate : Microsoft.EntityFrameworkCore.Migrations.Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)

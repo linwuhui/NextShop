d104b371df182aa832823d79953e7218b31dff8f
Breeze <2786515248@qq.com>
2024-09-20 00:27:30 +0800

    添加项目文件。

---
+++ NextShop.API/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using NextShop.API.Entities;
+
+namespace NextShop.API.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}

2ac109cc923fe801640d7f3b137b93fa9fd354c4
Breeze <2786515248@qq.com>
2024-09-20 14:26:05 +0800

    add ProductController and double get method remove unimportant files

---
+++ NextShop.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NextShop.API.Data;
+using NextShop.API.Entities;
+
+namespace NextShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _sStoreContext;
+
+        public ProductController(StoreContext context)
+        {
+            _sStoreContext = context;
+        }
+
+        [HttpGet("products")]
+        public ActionResult<List<Product>> GetProductList()
+        {
+            return Ok(_sStoreContext.Products.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return _sStoreContext.Products.Find(id)!;
+        }
+    }
+}
